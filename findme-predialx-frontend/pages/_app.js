d1bfd557edd1ea8191d14aaf3f2bc16de8b0ab6f
Mateus José Barbosa <mateus.barbosa@leucotron.com.br>
2021-05-03 17:23:46 -0300

    refactor: add eslint fix

--- findme-predialx-frontend/pages/_app.js
+++ findme-predialx-frontend/pages/_app.js
@@ -1,6 +1,11 @@
+import React from 'react';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
 import 'tailwindcss/tailwind.css';
 
+// eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
 
