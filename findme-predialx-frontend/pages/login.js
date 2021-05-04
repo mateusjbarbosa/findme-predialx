08740cad26b426f65dd0ee0fd88c8cf825f3a1ad
Mateus José Barbosa <mateus.barbosa@leucotron.com.br>
2021-05-03 23:06:37 -0300

    feat: add login form on page one

---
+++ findme-predialx-frontend/pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PageWrapper from '../src/components/wrappers/PageWrapper';
+
+import LoginForm from '../src/components/Forms/Login';
+
+export default function LoginPage() {
+  return (
+    <PageWrapper>
+      <LoginForm />
+    </PageWrapper>
+  );
+}
