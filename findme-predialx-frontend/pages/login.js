24784aaa2f8294384a873f4d2f5f2220d376069f
Mateus José Barbosa <mateus.barbosa@leucotron.com.br>
2021-05-04 13:41:37 -0300

    feat: add session verification on index and login page

--- findme-predialx-frontend/pages/login.js
+++ findme-predialx-frontend/pages/login.js
@@ -4,10 +4,21 @@ import PageWrapper from '../src/components/wrappers/PageWrapper';
 
 import LoginForm from '../src/components/Forms/Login';
 
-export default function LoginPage() {
+import { verifySession } from '../src/services/Auth';
+
+// eslint-disable-next-line react/prop-types
+export default function LoginPage({ allowedRoutes, username }) {
   return (
-    <PageWrapper>
+    <PageWrapper allowedRoutes={allowedRoutes} username={username}>
       <LoginForm />
     </PageWrapper>
   );
+}
+
+export async function getServerSideProps(context) {
+  const result = await verifySession(context);
+
+  return {
+    ...result,
+  };
 }
