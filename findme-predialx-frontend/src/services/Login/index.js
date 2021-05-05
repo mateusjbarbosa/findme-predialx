6124556dd7ddf56bea6db3ab23f83e0ab83062bb
Mateus José Barbosa <mateus.barbosa@leucotron.com.br>
2021-05-05 11:34:36 -0300

    refactor: create http infra file to export http client

--- findme-predialx-frontend/src/services/Login/index.js
+++ findme-predialx-frontend/src/services/Login/index.js
@@ -2,35 +2,13 @@ import { setCookie, destroyCookie } from 'nookies';
 
 import jwt from 'jsonwebtoken';
 
-const BACKEND_URL = 'http://localhost:3000';
-
-export const LOGIN_APP_TOKEN = 'LOGIN_APP_TOKEN';
-
-async function HttpClient(url, { headers, body, ...options }) {
-  return fetch(url, {
-    headers: {
-      ...headers,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-    ...options,
-  }).then(async (serverResponse) => {
-    if (serverResponse.ok) {
-      if (serverResponse.status === 204) return '';
-      return serverResponse.json();
-    }
-
-    throw new Error('Error on get server response.');
-  });
-}
+import { HttpClient, BACKEND_URL, LOGIN_APP_TOKEN } from '../Infra';
 
 const loginService = {
   async login(
     { username, password },
-    setCookieModule = setCookie,
-    HttpClientModule = HttpClient,
   ) {
-    return HttpClientModule(`${BACKEND_URL}/auth/login`, {
+    return HttpClient(`${BACKEND_URL}/auth/login`, {
       method: 'POST',
       body: {
         username,
@@ -44,7 +22,7 @@ const loginService = {
 
       const MAX_AGE_TOKEN = 30 * 60; // 30 minutos
 
-      setCookieModule(null, LOGIN_APP_TOKEN, token, {
+      setCookie(null, LOGIN_APP_TOKEN, token, {
         path: '/',
         maxAge: MAX_AGE_TOKEN,
       });
@@ -53,14 +31,12 @@ const loginService = {
 
   async logout(
     token,
-    destroyCookieModule = destroyCookie,
-    HttpClientModule = HttpClient,
   ) {
-    destroyCookieModule(null, LOGIN_APP_TOKEN);
+    destroyCookie(null, LOGIN_APP_TOKEN);
 
     const data = jwt.decode(token);
 
-    return HttpClientModule(`${BACKEND_URL}/auth/logout/${data.sub}`, {
+    return HttpClient(`${BACKEND_URL}/auth/logout/${data.sub}`, {
       method: 'POST',
       headers: {
         authorization: `Bearer ${token}`,
