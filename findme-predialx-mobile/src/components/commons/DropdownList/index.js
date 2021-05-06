02d0e280cfec73441ee0ecea40f8a2ebdfec69db
Mateus José Barbosa <mateus.barbosa@leucotron.com.br>
2021-05-06 20:03:40 -0300

    feat: create modal selector component

---
+++ findme-predialx-mobile/src/components/commons/DropdownList/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import ModalSelector from 'react-native-modal-selector';
+
+export const DropdownList = ({ data, initValue, onChange }) => {
+  return (
+    <ModalSelector
+      data={data}
+      initValue={initValue}
+      onChange={(value) => onChange(value.label)}
+      cancelText="cancelar escolha"
+      cancelStyle={{
+        padding: 10,
+        backgroundColor: 'rgba(239, 68, 68, 1)',
+      }}
+      cancelTextStyle={{
+        color: 'white',
+        fontWeight: 'bold',
+      }}
+      optionContainerStyle={{
+        backgroundColor: 'white',
+      }}
+      optionStyle={{
+        backgroundColor: 'white',
+      }}
+      optionTextStyle={{
+        color: 'rgba(107, 114, 128, 1)',
+        fontWeight: 'bold',
+      }}
+      selectStyle={{
+        borderRadius: 5,
+
+        backgroundColor: 'rgba(229, 231, 235, 0.5)',
+      }}
+      selectedItemTextStyle={{
+        color: 'rgba(229, 231, 235, 1)',
+      }}
+      initValueTextStyle={{
+        fontWeight: 'bold',
+        color: 'rgba(107, 114, 128, 1)',
+      }}
+    />
+  );
+};
